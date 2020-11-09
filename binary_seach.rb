5d8caa7ab08b8b7374beef666aa864189e4eb520
重田 将人 <masato.0928.j@gmail.com>
2020-11-09 20:22:34 +0900

    二分探索を実装

---
+++ binary_seach.rb
@@ -0,0 +1,20 @@
+# 二分探索
+def binary_seach(arr, target)
+    arr.sort!
+    head = arr.size
+    tail = 0
+
+    while head != tail
+        mid = (head + tail)/2
+        if arr[mid] == target
+            return mid
+        elsif arr[mid] < target
+            tail = mid + 1
+        else
+            head = mid - 1
+        end
+    end
+end
+
+arr = [1,3,4,5,7,8,9,11]
+p binary_seach(arr, 4)
